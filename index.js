b42e077f1f8ff4a068164758042c30c2dd68238f
Jasir PK <jasir@tathkarah.in>
2026-05-25 21:48:32 +0530

    server created (core module example)

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log('Hello, World!');
-console.log("this is sample code");
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello from http server');
+}).listen(8080);
+
+
+
+
+
---
+++ modules.js
@@ -0,0 +1,3 @@
+function sayHello() {
+    console.log("hello Good morning");
+}
