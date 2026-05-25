c3e269643543ea16bf98f9872ac83ef9127982b0
Jasir PK <jasijasu959@gmail.com>
2026-05-25 22:20:24 +0530

    function anonymus and objects are created

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-var http = require('http');
+// var http = require('http');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello from http server');
-}).listen(8080);
-
-
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('Hello from http server');
+// }).listen(8080);
 
 
 
+// const sayHello = require('./modules');
+
+const hello = require('./modules');
+// hello();
+
+console.log(hello.name);
+console.log(hello.age);
+console.log(hello.city);
+hello.greet();
