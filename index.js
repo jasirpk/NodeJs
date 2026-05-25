acda948059f359a6e1bca80e6a6c04de7f665301
Jasir PK <jasijasu959@gmail.com>
2026-05-25 23:03:30 +0530

    npm (third party packages installed)

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ console.log(hello.name);
 console.log(hello.age);
 console.log(hello.city);
 hello.greet();
+
+
+var cl = require('cli-color');
+
+console.log(cl.red('This is red text'));
+console.log(cl.green('This is green text'));
+console.log(cl.blue.underline('This is blue text'));
+
+var uc = require('upper-case');
+
+console.log(uc.upperCase('hello world'));
+console.log(uc.upperCase('jasir'));
