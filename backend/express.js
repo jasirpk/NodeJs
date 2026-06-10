b0c47b15adf16e469a206b051fd7c1c0d5b675bf
Jasir PK <jasijasu959@gmail.com>
2026-06-10 23:41:34 +0530

    express routing completed

--- backend/express.js
+++ backend/express.js
@@ -3,11 +3,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/api/data', (req, res)=>{
+app.get('/', (req, res)=>{
 
-    res.json({ message: "Hello from the backend!"});
+    res.send("Welcome to the Express Server");
+})
+
+app.get('/product', (req, res)=>{
+    res.send("This is the product page");
+})
+
+app.get('/about', (req, res)=>{
+    res.send("This is the about page");
+})
+
+app.get('/contact', (req, res)=>{
+    res.send("This is the contact page");
 })
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`);
 })
+
