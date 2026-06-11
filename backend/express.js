0b3d8ee7ab3b02f4bb2fe33562acb640dc9c9656
Jasir PK <jasijasu959@gmail.com>
2026-06-11 15:13:23 +0530

    html file send in response

--- backend/express.js
+++ backend/express.js
@@ -1,23 +1,27 @@
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res)=>{
-
-    res.send("Welcome to the Express Server");
+    // res.send("This is the home page");
+    res.sendFile(path.join(__dirname , 'index.html'));
 })
 
 app.get('/product', (req, res)=>{
-    res.send("This is the product page");
+    // res.send("This is the product page");
+    res.sendFile(path.join(__dirname , 'product.html'));
 })
 
 app.get('/about', (req, res)=>{
-    res.send("This is the about page");
+    // res.send("This is the about page");
+    res.sendFile(path.join(__dirname , 'about.html'));
 })
 
 app.get('/contact', (req, res)=>{
-    res.send("This is the contact page");
+    // res.send("This is the contact page");
+    res.sendFile(path.join(__dirname , 'contact.html'));
 })
 
 app.listen(port, ()=>{
