917cb7fb56d7a5e581edaf60d3d58cd4db2f468f
Jasir PK <jasijasu959@gmail.com>
2026-06-11 20:29:23 +0530

    middleware implemented

--- backend/express.js
+++ backend/express.js
@@ -2,27 +2,29 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.get('/', (req, res)=>{
-    // res.send("This is the home page");
-    res.sendFile(path.join(__dirname , 'index.html'));
-})
+app.use(express.static('public'));
 
-app.get('/product', (req, res)=>{
-    // res.send("This is the product page");
-    res.sendFile(path.join(__dirname , 'product.html'));
-})
+// app.get('/', (req, res)=>{
+//     // res.send("This is the home page");
+//     res.sendFile(path.join(__dirname , 'index.html'));
+// })
 
-app.get('/about', (req, res)=>{
-    // res.send("This is the about page");
-    res.sendFile(path.join(__dirname , 'about.html'));
-})
+// app.get('/product', (req, res)=>{
+//     // res.send("This is the product page");
+//     res.sendFile(path.join(__dirname , 'product.html'));
+// })
 
-app.get('/contact', (req, res)=>{
-    // res.send("This is the contact page");
-    res.sendFile(path.join(__dirname , 'contact.html'));
-})
+// app.get('/about', (req, res)=>{
+//     // res.send("This is the about page");
+//     res.sendFile(path.join(__dirname , 'about.html'));
+// })
+
+// app.get('/contact', (req, res)=>{
+//     // res.send("This is the contact page");
+//     res.sendFile(path.join(__dirname , 'contact.html'));
+// })
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`);
